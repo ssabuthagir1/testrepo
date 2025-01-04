48861f61591ad38bc2942e5a51871b26aa4fcf08
ssabuthagir1 <ssabuthagir@gmail.com>
2025-01-04 19:58:22 +0000

    changes in build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASS docker.io
-docker build -t jenweb .
-docker tag jenweb:latest shaikabuthagirs/ngi:jenweb
-docker push shaikabuthagirs/ngi:jenweb
-docker rmi -f jenweb
-docker pull shaikabuthagirs/ngi:jenweb
-docker run -itd --jenwebsrv -p "8081:80" jenweb
+docker build -t testweb2 .
+docker tag testweb2:latest shaikabuthagirs/ngi:testweb2
+docker push shaikabuthagirs/ngi:testweb2
+docker rmi -f testweb2
+docker pull shaikabuthagirs/ngi:testweb2
+docker run -itd --name testwebsrv -p "8081:80" testweb2
 
